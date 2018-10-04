126a809937afe953484b73a1e49b6a70b602e9c5
Matthew Wilkes <tonicblue@gmail.com>
2018-10-04 13:29:14 +0100

    Updating data urls to lower resolution versions

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ module.exports = {
   postcodeBatchSize: 5,
   postcodeProgressChunkSize: 10000,
   districtsUrl: 'https://opendata.arcgis.com/datasets/593018bf59ab4699b66355bd33cd186d_4.geojson',
-  wardsUrl: 'https://opendata.arcgis.com/datasets/07194e4507ae491488471c84b23a90f2_2.geojson',
+  wardsUrl: 'https://opendata.arcgis.com/datasets/07194e4507ae491488471c84b23a90f2_3.geojson',
   nutsLookupUrl: 'https://opendata.arcgis.com/datasets/2a2548641a294734ba4fdb689b12d955_0.geojson',
-  nutsUrl: 'https://opendata.arcgis.com/datasets/48b6b85bb7ea43699ee85f4ecd12fd36_2.geojson',
+  nutsUrl: 'https://opendata.arcgis.com/datasets/48b6b85bb7ea43699ee85f4ecd12fd36_3.geojson',
   postcodesUrl: 'https://opendata.arcgis.com/datasets/75edec484c5d49bcadd4893c0ebca0ff_0.csv?session=undefined&outSR=%7B%22wkid%22%3A27700%2C%22latestWkid%22%3A27700%7D'
 }
